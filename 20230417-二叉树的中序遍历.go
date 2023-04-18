88816f52dd0bd12128d9df6bbc10e9aa4edb479d
yuzixun <yzxun@qq.com>
2023-04-18 22:51:54 +0800

    add

--- 20230417-二叉树的中序遍历.go
+++ 20230417-二叉树的中序遍历.go
@@ -1,1 +1,25 @@
 package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) (res []int) {
+	var inorder func(root *TreeNode)
+
+	inorder = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		inorder(root.Left)
+		res = append(res, root.Val)
+		inorder(root.Right)
+	}
+
+	inorder(root)
+	return
+}
